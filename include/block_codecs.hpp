9d95668ca4a26c6de7b6c9166882ed65277c1009
Victorio Scafati <mr.grassho@gmail.com>
2021-01-25 19:25:52 -0300

    Update code_blocks
    Add functions bitsize(), posting_cost() and write()

--- include/block_codecs.hpp
+++ include/block_codecs.hpp
@@ -158,6 +158,7 @@ struct interpolative_block {
 
 struct varintg8iu_block {
     static const uint64_t block_size = constants::block_size;
+    static const int type = 1;
 
     struct codec_type : VarIntG8IU {
         // rewritten version of decodeBlock optimized for when the output
@@ -186,6 +187,56 @@ struct varintg8iu_block {
             return readSize;
         }
     };
+
+    static inline uint64_t posting_cost(posting_type x, uint64_t base) {
+        // TODO - Check
+        if (x == 0 or x - base == 0) {
+            return 8;
+        }
+
+        assert(x >= base);
+        return 8 *
+               succinct::util::ceil_div(ceil_log2(x - base + 1),  // delta gap
+                                        7);
+    }
+
+    template <typename Iterator>
+    static DS2I_FLATTEN_FUNC uint64_t bitsize(Iterator begin,
+                                              global_parameters const& params,
+                                              uint64_t universe, uint64_t n,
+                                              uint64_t base = 0) {
+        // TODO - Check
+        (void)params;
+        (void)universe;
+        uint64_t cost = 0;
+        auto it = begin;
+        for (uint64_t i = 0; i < n; ++i, ++it) {
+            cost += posting_cost(*it, base);
+            base = *it;
+        }
+        return cost;
+    }
+
+    template <typename Iterator>
+    static void write(succinct::bit_vector_builder& bvb, Iterator begin,
+                      uint64_t base, uint64_t universe, uint64_t n,
+                      global_parameters const& params) {
+        // TODO - Check
+        (void)params;
+        std::vector<uint32_t> gaps;
+        gaps.reserve(n);
+        uint32_t last_doc(-1);
+        for (size_t i = 0; i < n; ++i) {
+            uint32_t doc = *(begin + i) - base;
+            gaps.push_back(doc - last_doc);  // delta gap
+            last_doc = doc;
+        }
+        std::vector<uint8_t> out;
+        encode(gaps.data(), universe, n, out);
+        for (uint8_t v : out) {
+            bvb.append_bits(v, 8);
+        }
+    }
 
     static void encode(uint32_t const* in, uint32_t sum_of_values, size_t n,
                        std::vector<uint8_t>& out) {
