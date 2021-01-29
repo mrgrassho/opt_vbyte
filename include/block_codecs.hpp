0006de1fdcc8530dd04702a13c43843376ce6cb5
Victorio Scafati <mr.grassho@gmail.com>
2021-01-29 17:09:43 -0300

    Add support to VarIntGB

--- include/block_codecs.hpp
+++ include/block_codecs.hpp
@@ -198,7 +198,8 @@ struct varintg8iu_block {
         }
         codec_type varint_codec;
         assert(x >= base);
-        return (uint64_t) varint_codec.posting_cost(x, base);
+        // val = size(x-base) bytes + header = 1 bit
+        return (uint64_t) varint_codec.posting_cost(x, base) + 1;
     }
 
     template <typename Iterator>
@@ -393,6 +394,55 @@ struct maskedvbyte_block {
 
 struct varintgb_block {
     static const uint64_t block_size = constants::block_size;
+
+    static inline uint64_t posting_cost(posting_type x, uint64_t base) {
+        if (x == 0 or x - base == 0) {
+            return 8;
+        }
+        assert(x >= base);
+        // val = size(x-base) bytes
+        uint64_t val = (uint64_t) (((__builtin_clz( (uint32_t) x - base | 255) ^ 31) >> 3) + 1) * 8;
+        // header = 2 bits
+        return val + 2;
+    }
+
+    template <typename Iterator>
+    static DS2I_FLATTEN_FUNC uint64_t bitsize(Iterator begin,
+                                              global_parameters const& params,
+                                              uint64_t universe, uint64_t n,
+                                              uint64_t base = 0) {
+        // TODO - Check
+        (void)params;
+        (void)universe;
+        uint64_t cost = 0;
+        auto it = begin;
+        for (uint64_t i = 0; i < n; ++i, ++it) {
+            cost += posting_cost(*it, base);
+            base = *it;
+        }
+        return cost;
+    }
+
+    template <typename Iterator>
+    static void write(succinct::bit_vector_builder& bvb, Iterator begin,
+                      uint64_t base, uint64_t universe, uint64_t n,
+                      global_parameters const& params) {
+        // TODO - Check
+        (void)params;
+        std::vector<uint32_t> gaps;
+        gaps.reserve(n);
+        uint32_t last_doc(-1);
+        for (size_t i = 0; i < n; ++i) {
+            uint32_t doc = *(begin + i) - base;
+            gaps.push_back(doc - last_doc);  // delta gap
+            last_doc = doc;
+        }
+        std::vector<uint8_t> out;
+        encode(gaps.data(), universe, n, out);
+        for (uint8_t v : out) {
+            bvb.append_bits(v, 8);
+        }
+    }
 
     static void encode(uint32_t const* in, uint32_t sum_of_values, size_t n,
                        std::vector<uint8_t>& out) {
